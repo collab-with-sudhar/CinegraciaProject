1f96cd6859c5b6fea9d2bc9e4dbf616ae5c38dfc
sudharcruz <88190543+sudharcruz@users.noreply.github.com>
2023-08-06 18:41:26 +0530

    white

--- webproject/settings.py
+++ webproject/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nonstatic',
     'websiteapp',
     'register',
     'moviedetail',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
