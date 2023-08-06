1fff99ee3558257b47a33277c1660b78d0c5c43c
sudharcruz <88190543+sudharcruz@users.noreply.github.com>
2023-08-06 18:46:23 +0530

    new white

--- webproject/settings.py
+++ webproject/settings.py
@@ -18,6 +18,9 @@ import os
 
 import dj_database_url
 
+from whitenoise import runserver_nostatic
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
