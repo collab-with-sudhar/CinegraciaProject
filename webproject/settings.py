04c92c6980a41fe75c397f5372828d0d99ba7cde
sudharcruz <sudharsandevaraj1219@gmail.com>
2023-08-07 13:16:51 +0530

    kom

--- webproject/settings.py
+++ webproject/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b)=9blj9f6u8giolw1mcj+uc27+)1mm-xd2^3hm&9_j+y5xy0)'
 #SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
