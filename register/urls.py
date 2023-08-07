1e9871e76e32350e075fb0af35a8725ee34ec2a7
sudharcruz <sudharsandevaraj1219@gmail.com>
2023-08-07 11:52:44 +0530

    newest changes

--- register/urls.py
+++ register/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , re_path
 from .import views
 from django.contrib.auth import views as auth_views
 from .views import *
+from django.conf import settings
+
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('register/',views.register, name='register'),
     path('login',views.login,name='login'),
     path('logout',views.logout,name='logout'),
